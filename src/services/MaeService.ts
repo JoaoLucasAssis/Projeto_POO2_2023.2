1cf2fb8c7ce47dc94576e150b718d00fed9b5dba
VitorLugon <vitorlugon@hotmail.com.br>
2023-09-01 15:26:36 -0300

    Projeto em andamento

---
+++ src/services/MaeService.ts
@@ -0,0 +1,41 @@
+// import { PrismaClient } from "@prisma/client";
+// import Mae from "../models/mae";
+
+// const prisma = new PrismaClient();
+
+// class UserServices{
+//     constructor(){
+
+//     }
+
+//     async insertMae(mae: Mae){
+//         try{
+//             const newMae = await prisma.user.create({ mae });
+
+//         } catch(error) {
+//             console.log(error);
+//         }
+//     }
+
+//     async updateMae(mae: Mae){
+//         try{
+
+//         const maes = await prisma.user.findMany();
+//         } catch(error) {
+//             console.log(error);
+//         }
+//     }
+
+//     async deleteMae(mae: Mae){
+//         try{
+
+//             const maes = await prisma.user.findMany();
+//         } catch(error) {
+//             console.log(error);
+//         }
+//     }
+
+// }
+
+
+// export default UserServices;
