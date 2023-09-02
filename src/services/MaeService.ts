2773cd28a7938cbaf17589ff62025d23ad33465d
João Lucas <103143674+JoaoLucasAssis@users.noreply.github.com>
2023-09-02 16:40:13 -0300

    adicionado arquivo 'MaeService.ts' a pasta de serviços da atividade

--- src/services/MaeService.ts
+++ src/services/MaeService.ts
@@ -1,41 +1,41 @@
-// import { PrismaClient } from "@prisma/client";
-// import Mae from "../models/mae";
+import { PrismaClient } from "@prisma/client";
+import Mae from "../models/mae";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// class UserServices{
-//     constructor(){
+class UserServices{
+    constructor(){
 
-//     }
+    }
 
-//     async insertMae(mae: Mae){
-//         try{
-//             const newMae = await prisma.user.create({ mae });
+    async insertMae(mae: Mae){
+        try{
+            const newMae = await prisma.user.create({ mae });
 
-//         } catch(error) {
-//             console.log(error);
-//         }
-//     }
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
-//     async updateMae(mae: Mae){
-//         try{
+    async updateMae(mae: Mae){
+        try{
 
-//         const maes = await prisma.user.findMany();
-//         } catch(error) {
-//             console.log(error);
-//         }
-//     }
+        const maes = await prisma.user.findMany();
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
-//     async deleteMae(mae: Mae){
-//         try{
+    async deleteMae(mae: Mae){
+        try{
 
-//             const maes = await prisma.user.findMany();
-//         } catch(error) {
-//             console.log(error);
-//         }
-//     }
+            const maes = await prisma.user.findMany();
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
-// }
+}
 
 
-// export default UserServices;
+export default UserServices;
