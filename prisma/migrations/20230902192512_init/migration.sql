c4fee1493d449737b51028c6f457fa5dc73cdccb
joaolucas <joaolucasdeassis.o@gmail.com>
2023-09-02 16:35:25 -0300

    adicionado pasta de versioanmento de migrações do arquivo 'schema.prisma'

---
+++ prisma/migrations/20230902192512_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Mae" (
+    "RG" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "data" TEXT,
+    "endereco" TEXT,
+    "telefone" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Medico" (
+    "crm" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT,
+    "endereco" TEXT,
+    "especialidade" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Bebe" (
+    "certidao_nascimento" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "data" TEXT,
+    "altura" REAL,
+    "peso" REAL,
+    CONSTRAINT "Bebe_certidao_nascimento_fkey" FOREIGN KEY ("certidao_nascimento") REFERENCES "Medico" ("crm") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Bebe_certidao_nascimento_fkey" FOREIGN KEY ("certidao_nascimento") REFERENCES "Mae" ("RG") ON DELETE RESTRICT ON UPDATE CASCADE
+);
